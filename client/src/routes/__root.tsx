d637f196f262a7c1fec555bc5041aff57251d1f1
mohammed-safaan <alisafaan2015@gmail.com>
2025-05-03 18:54:50 +0300

    feat(client): :sparkles: add a theme toggler button

--- client/src/routes/__root.tsx
+++ client/src/routes/__root.tsx
@@ -1,11 +1,16 @@
+import { ModeToggle } from '@/components/mode-toggle'
+import { ThemeProvider } from '@/components/theme-provider'
 import { Outlet, createRootRoute } from '@tanstack/react-router'
 import { TanStackRouterDevtools } from '@tanstack/react-router-devtools'
 
 export const Route = createRootRoute({
   component: () => (
-    <>
+    <ThemeProvider>
+      <div className="fixed top-3 right-3 z-50 flex items-center justify-end gap-2">
+        <ModeToggle />
+      </div>
       <Outlet />
       <TanStackRouterDevtools />
-    </>
+    </ThemeProvider>
   ),
 })
